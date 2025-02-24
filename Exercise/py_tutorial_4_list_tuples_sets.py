cbc743d9cfb7c5c75a71eb2f99ba5ce0c9c1a567
Htet-Khant-Linn <htetkhantlinn123@gmail.com>
2025-02-24 21:20:47 +0630

    Updating Tutorial 4

--- Exercise/py_tutorial_4_list_tuples_sets.py
+++ Exercise/py_tutorial_4_list_tuples_sets.py
@@ -57,3 +57,57 @@ print(course_str)
 
 new_list = course_str.split(', ')    # This will split the string into a list
 print(new_list)
+
+
+# mutable
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+
+print(list_1)
+print(list_2)
+list_1[0]   = 'Art'
+print(list_1)
+print(list_2)
+
+# immutable
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+print(tuple_1)
+print(tuple_2)
+
+tuple_1[0] = 'Art'    # This will throw an error because tuples are immutable
+print(tuple_1)
+print(tuple_2)
+
+
+# Sets
+cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'Math'}  # This is a set
+# sets do not have order, set throw away duplicates
+print(cs_courses)
+print('Math' in cs_courses) # Set are optimize for this
+
+art_courses = {'History', 'Math', 'Art', 'Design'}
+print(cs_courses.intersection(art_courses))    # This will print the intersection of the two sets
+print(cs_courses.difference(art_courses))    # This will print the difference between the two sets
+print(cs_courses.union(art_courses))    # This will print the union of the two sets
+
+
+list_3 = [1, 3, 5, 7, 9, 1, 3, 5, 7, 9]
+list_4 = [2, 5, 7, 7, 3, 5]
+print(list_3.intersection(list_4))     # This will throw an error because lists do not have the method 'intersection'
+print(set(list_3).intersection(list_4))    # This will print the intersection of the two lists
+print(set(list_3).difference(list_4))    # This will print the difference between the two lists
+
+
+# Empty Lists
+empty_list = []
+empty_list = list()
+
+# Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
+
+# Empty Sets
+empty_set = {}    # This will create a dictionary
+empty_set = set()    # This will create an empty set
+
